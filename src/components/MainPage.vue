47ec1ba7eb051861da6729b6d2f221e0c7a5ec95
SerPan23 <igracom@yandex.ru>
2020-08-29 19:28:02 +0300

    Update MainPage.vue

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,24 +1,34 @@
 <template>
     <div class="main">
-        Главная страница
+        <div class="warp">
+            Главная страница
+        </div>
+        <div class="footer"><Footer></Footer></div> 
     </div>
 </template>
 
 <script>
+import Footer from './footer.vue'
 export default {
     name: 'MainPage',
+    components: { Footer },
 }
 </script>
 
 <style scoped>
+.warp{
+    flex: 1 0 auto;
+    padding-top: 150px !important;
+}
+.footer{
+    flex: 0 0 auto;
+}
 .main{
     display: flex;
 	flex-direction: column;
-}
-.main{
+    min-height: 100vh;
     height: 100%;
     padding: 0px 0px;
-    min-height: 100vh;
     margin-bottom: 0px;
 }
 </style>
